bae7faa8f247f9894d22cb8917baedcd0cb38efd
Артем Белышев <artembelysev@MBP-Artem.Dlink>
2020-07-01 16:42:46 +0300

    cr

--- src/components/hidden.js
+++ src/components/hidden.js
@@ -9,4 +9,6 @@
     "version": "7.0.0-beta.0"
   }
 ]
-https://dashboard.heroku.com/apps/vast-tundra-39777/api/profile
+https://vast-tundra-39777.herokuapp.com/api/smash
+http://127.0.0.1:8888/api/smash
+CONNECTION_URI || ;
