c14357e7e5fae4f70e98065417ade5f8603f2426
Артем Белышев <artembelysev@MBP-Artem.Dlink>
2020-07-03 17:20:36 +0300

    cr

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Main from './Main'
+import Main from './Main';
+import { Route } from 'react-router-dom';
 import spaceman from '../../public/images/astronaut-3.svg';
 import '../../public/styles/style2.css';
 
 class Header extends React.Component {
-  
+
 
 render() {
   return (
 
     <div className="photo">
       <img src={spaceman} id='space' />
-       <Main />
+      <Route exact path={['/', '/hi', '/ho', '/telegram']} component={Main} /> 
     </div>
 
   )
